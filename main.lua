d055d3d0b8920347898c66abaa6c01ff3db1b464
Steven Degutis <sbdegutis@gmail.com>
2021-11-21 13:18:07 -0600

    Completely wrong way to do buttons.

--- main.lua
+++ main.lua
@@ -4,26 +4,71 @@ require('lib.util').setWindowSize(800, 600)
 
 -- local enet = require('enet')
 local loadP8 = require('lib.pico8')
+
 love.graphics.setFont(loadP8('res/font.p8').createFont())
 
-local widgetRes = loadP8('res/widgets.p8')
+local Button = {}
 
-local function drawButton(x,y,scale,spr,text)
-  local left = widgetRes.getOrMakeSpriteAt(spr+0)
-  local mid = widgetRes.getOrMakeSpriteAt(spr+1)
-  local right = widgetRes.getOrMakeSpriteAt(spr+2)
+function Button.new(p8, x, y, scale, spr, text)
+  return setmetatable({
+    x=x,y=y,
+    scale=scale,
+    spr=spr,
+    text=text,
+    spritesUp = {
+      left  = p8.getOrMakeSpriteAt(spr+0),
+      mid   = p8.getOrMakeSpriteAt(spr+1),
+      right = p8.getOrMakeSpriteAt(spr+2),
+    },
+    spritesDown = {
+      left  = p8.getOrMakeSpriteAt(spr+0+16),
+      mid   = p8.getOrMakeSpriteAt(spr+1+16),
+      right = p8.getOrMakeSpriteAt(spr+2+16),
+    },
+  }, {__index=Button})
+end
 
-  left:draw(x,y,scale)
-  mid:draw(x+(8*scale),y,scale)
-  right:draw(x+(16*scale),y,scale)
+function Button:draw()
+  local sprites = self.spritesUp
+  local x = self.x
+  local y = self.y
+  local scale = self.scale
+  local text = self.text
+
+  local numSprs = #text/2 + 1 -- 2 chars per sprite, - 1 sprite total for left/right padding
+  local eachSprPixels = 8*scale
 
   love.graphics.setColor(1,1,1)
+
+  for i=0,numSprs-1 do
+    sprites.mid:draw(x+eachSprPixels*i,y,scale)
+  end
+
+  sprites.left:draw(x,y,scale)
+  sprites.right:draw(x+(eachSprPixels*(numSprs-1.5)),y,scale)
+
+  love.graphics.setColor(0,0,0)
   love.graphics.print(text, x+(2*scale),y+(2*scale),0,scale,scale)
 end
 
-function love.draw()
-  love.graphics.clear()
-
+function Button:update()
   local mx, my = love.mouse.getPosition()
-  drawButton(mx-10, my-50, 5, 0, "testing")
+
+  
+end
+
+local widgetRes = loadP8('res/widgets.p8')
+
+local b1 = Button.new(widgetRes, 10,10, 5, 0, "Testing this!")
+local b2 = Button.new(widgetRes, 130,150, 3, 0, "yep")
+
+function love.draw()
+  love.graphics.clear(0,0.15,0)
+  b1:draw()
+  b2:draw()
+end
+
+function love.update()
+  b1:update()
+  b2:update()
 end
