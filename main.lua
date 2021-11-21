f0a1b857b724b04490707e36b999fb02d18590ea
Steven Degutis <sbdegutis@gmail.com>
2021-11-21 10:09:30 -0600

    Slight font fixes.

--- main.lua
+++ main.lua
@@ -21,12 +21,20 @@ end
 
 function love.draw()
   love.graphics.clear()
-  love.graphics.setColor(0,0.5,0.5)
-  love.graphics.rectangle('fill',10,10,200,35)
-  love.graphics.setColor(0,1,1)
-  love.graphics.rectangle('line',10,10,200,35)
+  -- love.graphics.setColor(0,0.5,0.5)
+  -- love.graphics.rectangle('fill',10,10,200,35)
+  -- love.graphics.setColor(0,1,1)
+  -- love.graphics.rectangle('line',10,10,200,35)
   -- widgetRes.getOrMakeSpriteAt(1):draw(  20*3, 10, 4)
 
   love.graphics.setColor(1,1,0)
-  love.graphics.print("ACBDB\nBACD", 14,14,0,4,4)
+  love.graphics.print("hello world!\n\n"..
+                      "a+b=c\n\n"..
+                      "[char]\n\n"..
+                      "(what?)\n\n"..
+                      "You know; it's great!\n\n"..
+                      "Or is it?\n\n"..
+                      "Yes, it is.\n\n"..
+                      "12 + 34 / 56 - 78 * 90 = 30%\n\n"..
+                      "", 14,14,0,4,4)
 end
